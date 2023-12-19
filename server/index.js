9b34d40d81fd9c29f781dfb6ea3b7135cbfbaf94
Sri Hema <19epci022@skcet.ac.in>
2023-12-19 23:57:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,6 +24,18 @@ mongoose.connection.on('disconnected', () => {
   console.log('Disconnected from MongoDB');
 });
 app.post('/enroll', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
   const { name, age, batch } = req.body;
   const user = await User.create({ name, age, batch });
   res.json(user);
